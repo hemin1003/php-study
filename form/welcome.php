35d9fb6f36bcea6c507b8fbdf6960002ae5304fa
minbo <hemin_it@163.com>
2017-07-13 19:00:38 +0800

    update

--- form/welcome.php
+++ form/welcome.php
@@ -1,4 +1,17 @@
 欢迎
-<?php echo $_REQUEST["fname"] ?>
+<?php 
+echo $_REQUEST["fname"];
+echo "<br>";
+?>
 
-你的年龄是<?php echo $_REQUEST["age"]?>
+你的年龄是
+<?php 
+echo $_REQUEST["age"];
+
+try {
+    throw new Exception('This is my exception location.');
+}catch (Exception $e){
+    echo $e->getMessage();
+}
+
+?>
